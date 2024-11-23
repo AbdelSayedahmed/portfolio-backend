67c55bcf98d46f68a1b69e00dbc009b4280e331a
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-11-23 11:24:03 -0500

    redone app.js

--- controllers/dataControllers.js
+++ controllers/dataControllers.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const router = express.Router();
+const data = express.Router();
 const skills = require("../db/skills.json");
 const projects = require("../db/projects.json");
 const nanoid = require("nanoid");
 const fs = require("fs");
 
-module.exports = router;
+module.exports = data;
