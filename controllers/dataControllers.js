127944af9dda5990c5cd1c43ffb10003aed48bc0
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-11-23 11:20:22 -0500

    added seed id

--- controllers/dataControllers.js
+++ controllers/dataControllers.js
@@ -1,23 +1,8 @@
 const express = require("express");
-const data = express.Router();
-const { getAllProjects, getAllSkills } = require("../queries/dataQueries");
+const router = express.Router();
+const skills = require("../db/skills.json");
+const projects = require("../db/projects.json");
+const nanoid = require("nanoid");
+const fs = require("fs");
 
-data.get("/projects", async (req, res) => {
-  try {
-    const allProjects = await getAllProjects();
-    res.status(200).json(allProjects);
-  } catch {
-    res.status(500).json({ error: "Server error" });
-  }
-});
-
-data.get("/skills", async (req, res) => {
-  try {
-    const allSkills = await getAllSkills();
-    res.status(200).json(allSkills);
-  } catch {
-    res.status(500).json({ error: "Server error" });
-  }
-});
-
-module.exports = data;
+module.exports = router;
