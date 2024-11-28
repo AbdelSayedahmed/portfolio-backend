9b463745f052eb1d49b8060ddee9d83feb38e3fe
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-11-28 16:14:45 -0500

    controller updated

--- controllers/dataControllers.js
+++ controllers/dataControllers.js
@@ -11,4 +11,15 @@ data.get("/skills", (req, res) => res.status(200).json(skills));
 // Get all projects
 data.get("/projects", (req, res) => res.status(200).json(projects));
 
+// Get a single project
+data.get("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  const project = projects.find((project) => project.id === id);
+  if (project) {
+    res.status(200).json(project);
+  } else {
+    res.status(404).json({ message: "Project not found" });
+  }
+});
+
 module.exports = data;
